4a6188ce52c138ad5075fae0fb8f8c72f7b84489
Wersjon <Wersjon@users.noreply.github.com>
2019-12-18 16:55:08 +0100

    Troda 1.0_01 Alpha
    Fixes for wall display and moving

--- main.cpp
+++ main.cpp
@@ -64,38 +64,38 @@ void walls()
             isVertical = 0;
         break;
     }
-    if(map[X + 3 * isVertical - 4 * isUp * !isVertical][Y - 4 * isUp * isVertical - 3 * isUp * !isVertical] == '#') Troda.getValue("smalls/s3r");
-    if(map[X - 3 * isVertical - 4 * isUp * !isVertical][Y - 4 * isUp * isVertical + 3 * isUp * !isVertical] == '#') Troda.getValue("smalls/s3l");
-    if(map[X + 2 * isVertical - 4 * isUp * !isVertical][Y - 4 * isUp * isVertical - 3 * isUp * !isVertical] == '#') Troda.getValue("smalls/s2r");
-    if(map[X - 2 * isVertical - 4 * isUp * !isVertical][Y - 4 * isUp * isVertical + 3 * isUp * !isVertical] == '#') Troda.getValue("smalls/s2l");
-    if(map[X + isVertical - 4 * isUp * !isVertical][Y - 4 * isUp * isVertical - 3 * isUp * !isVertical] == '#') Troda.getValue("smalls/s1r");
-    if(map[X - isVertical - 4 * isUp * !isVertical][Y - 4 * isUp * isVertical + 3 * isUp * !isVertical] == '#') Troda.getValue("smalls/s1l");
+    if(map[X + isUp * 3 * isVertical - 4 * isUp * !isVertical][Y - 4 * isUp * isVertical - 3 * isUp * !isVertical] == '#') Troda.getValue("smalls/s3r");
+    if(map[X - isUp * 3 * isVertical - 4 * isUp * !isVertical][Y - 4 * isUp * isVertical + 3 * isUp * !isVertical] == '#') Troda.getValue("smalls/s3l");
+    if(map[X + isUp * 2 * isVertical - 4 * isUp * !isVertical][Y - 4 * isUp * isVertical - 3 * isUp * !isVertical] == '#') Troda.getValue("smalls/s2r");
+    if(map[X - isUp * 2 * isVertical - 4 * isUp * !isVertical][Y - 4 * isUp * isVertical + 3 * isUp * !isVertical] == '#') Troda.getValue("smalls/s2l");
+    if(map[X + isUp * isVertical - 4 * isUp * !isVertical][Y - 4 * isUp * isVertical - 3 * isUp * !isVertical] == '#') Troda.getValue("smalls/s1r");
+    if(map[X - isUp * isVertical - 4 * isUp * !isVertical][Y - 4 * isUp * isVertical + 3 * isUp * !isVertical] == '#') Troda.getValue("smalls/s1l");
     if(map[X - 4 * isUp * !isVertical][Y - 4 * isUp * isVertical] == '#') Troda.getValue("smalls/s1");
 
-    if(map[X + 3 * isVertical - 3 * isUp * !isVertical][Y - 3 * isUp * isVertical - 3 * isUp * !isVertical] == '#') Troda.getValue("ids/id17");
-    if(map[X - 3 * isVertical - 3 * isUp * !isVertical][Y - 3 * isUp * isVertical + 3 * isUp * !isVertical] == '#') Troda.getValue("ids/id16");
-    if(map[X + 2 * isVertical - 3 * isUp * !isVertical][Y - 3 * isUp * isVertical - 2 * isUp * !isVertical] == '#') Troda.getValue("ids/id15");
-    if(map[X - 2 * isVertical - 3 * isUp * !isVertical][Y - 3 * isUp * isVertical + 2 * isUp * !isVertical] == '#') Troda.getValue("ids/id14");
-    if(map[X + isVertical - 3 * isUp * !isVertical][Y - 3 * isUp * isVertical - isUp * !isVertical] == '#') Troda.getValue("ids/id13");
-    if(map[X - isVertical - 3 * isUp * !isVertical][Y - 3 * isUp * isVertical + isUp * !isVertical] == '#') Troda.getValue("ids/id12");
+    if(map[X + isUp * 3 * isVertical - 3 * isUp * !isVertical][Y - 3 * isUp * isVertical - 3 * isUp * !isVertical] == '#') Troda.getValue("ids/id17");
+    if(map[X - isUp * 3 * isVertical - 3 * isUp * !isVertical][Y - 3 * isUp * isVertical + 3 * isUp * !isVertical] == '#') Troda.getValue("ids/id16");
+    if(map[X + isUp * 2 * isVertical - 3 * isUp * !isVertical][Y - 3 * isUp * isVertical - 2 * isUp * !isVertical] == '#') Troda.getValue("ids/id15");
+    if(map[X - isUp * 2 * isVertical - 3 * isUp * !isVertical][Y - 3 * isUp * isVertical + 2 * isUp * !isVertical] == '#') Troda.getValue("ids/id14");
+    if(map[X + isUp * isVertical - 3 * isUp * !isVertical][Y - 3 * isUp * isVertical - isUp * !isVertical] == '#') Troda.getValue("ids/id13");
+    if(map[X - isUp * isVertical - 3 * isUp * !isVertical][Y - 3 * isUp * isVertical + isUp * !isVertical] == '#') Troda.getValue("ids/id12");
     if(map[X - 3 * isUp * !isVertical][Y - 3 * isUp * isVertical] == '#') Troda.getValue("ids/id11");
 
-    if(map[X + 3 * isVertical - 2 * isUp * !isVertical][Y - 2 * isUp * isVertical - 3 * isUp * !isVertical] == '#') Troda.getValue("ids/Hh");
-    if(map[X - 3 * isVertical - 2 * isUp * !isVertical][Y - 2 * isUp * isVertical + 3 * isUp * !isVertical] == '#') Troda.getValue("ids/Gh");
-    if(map[X + 2 * isVertical - 2 * isUp * !isVertical][Y - 2 * isUp * isVertical - 2 * isUp * !isVertical] == '#') Troda.getValue("ids/id10");
-    if(map[X - 2 * isVertical - 2 * isUp * !isVertical][Y - 2 * isUp * isVertical + 2 * isUp * !isVertical] == '#') Troda.getValue("ids/id9");
-    if(map[X + isVertical - 2 * isUp * !isVertical][Y - 2 * isUp * isVertical - isUp * !isVertical] == '#') Troda.getValue("ids/id8");
-    if(map[X - isVertical - 2 * isUp * !isVertical][Y - 2 * isUp * isVertical + isUp * !isVertical] == '#') Troda.getValue("ids/id7");
+    if(map[X + isUp * 3 * isVertical - 2 * isUp * !isVertical][Y - 2 * isUp * isVertical - 3 * isUp * !isVertical] == '#') Troda.getValue("ids/Hh");
+    if(map[X - isUp * 3 * isVertical - 2 * isUp * !isVertical][Y - 2 * isUp * isVertical + 3 * isUp * !isVertical] == '#') Troda.getValue("ids/Gh");
+    if(map[X + isUp * 2 * isVertical - 2 * isUp * !isVertical][Y - 2 * isUp * isVertical - 2 * isUp * !isVertical] == '#') Troda.getValue("ids/id10");
+    if(map[X - isUp * 2 * isVertical - 2 * isUp * !isVertical][Y - 2 * isUp * isVertical + 2 * isUp * !isVertical] == '#') Troda.getValue("ids/id9");
+    if(map[X + isUp * isVertical - 2 * isUp * !isVertical][Y - 2 * isUp * isVertical - isUp * !isVertical] == '#') Troda.getValue("ids/id8");
+    if(map[X - isUp * isVertical - 2 * isUp * !isVertical][Y - 2 * isUp * isVertical + isUp * !isVertical] == '#') Troda.getValue("ids/id7");
     if(map[X - 2 * isUp * !isVertical][Y - 2 * isUp * isVertical] == '#') Troda.getValue("ids/id6");
 
-    if(map[X + 2 * isVertical - isUp * !isVertical][Y - isUp * isVertical - 2 * isUp * !isVertical] == '#') Troda.getValue("ids/id10h");
-    if(map[X - 2 * isVertical - isUp * !isVertical][Y - isUp * isVertical + 2 * isUp * !isVertical] == '#') Troda.getValue("ids/id9h");
-    if(map[X + isVertical - isUp * !isVertical][Y - isUp * isVertical - isUp * !isVertical] == '#') Troda.getValue("ids/id5");
-    if(map[X - isVertical - isUp * !isVertical][Y - isUp * isVertical + isUp * !isVertical] == '#') Troda.getValue("ids/id4");
+    if(map[X + isUp * 2 * isVertical - isUp * !isVertical][Y - isUp * isVertical - 2 * isUp * !isVertical] == '#') Troda.getValue("ids/id10h");
+    if(map[X - isUp * 2 * isVertical - isUp * !isVertical][Y - isUp * isVertical + 2 * isUp * !isVertical] == '#') Troda.getValue("ids/id9h");
+    if(map[X + isUp * isVertical - isUp * !isVertical][Y - isUp * isVertical - isUp * !isVertical] == '#') Troda.getValue("ids/id5");
+    if(map[X - isUp * isVertical - isUp * !isVertical][Y - isUp * isVertical + isUp * !isVertical] == '#') Troda.getValue("ids/id4");
     if(map[X - isUp * !isVertical][Y - isUp * isVertical] == '#') Troda.getValue("ids/id3");
     
-    if(map[X + 1 * isVertical][Y - 1 * isUp * !isVertical] == '#') Troda.getValue("ids/id2");
-    if(map[X - 1 * isVertical][Y + 1 * isUp * !isVertical] == '#') Troda.getValue("ids/id1");
+    if(map[X + isUp * isVertical][Y - isUp * !isVertical] == '#') Troda.getValue("ids/id2");
+    if(map[X - isUp * isVertical][Y + isUp * !isVertical] == '#') Troda.getValue("ids/id1");
     
     if(drawed == false)
     {
@@ -105,9 +105,9 @@ void walls()
     if(kbhit())
     {
         znak = getch();
+        drawed = false;
         if(znak == 'W' || znak == 'w')
         {
-            drawed = false;
             if(direction == 0 && map[X][Y-1] != '#')Y--;
             else if(direction == 1 && map[X+1][Y] != '#') X++;
             else if(direction == 2 && map[X][Y+1] != '#') Y++;
@@ -116,7 +116,6 @@ void walls()
         }
         else if(znak == 'S' || znak == 's')
         {
-            drawed = false;
             if(direction == 0 && map[X][Y+1] != '#') Y++;
             else if(direction == 1 && map[X-1][Y] != '#') X--;
             else if(direction == 2 && map[X][Y-1] != '#') Y--;
@@ -125,19 +124,17 @@ void walls()
         }
         else if(znak == 'A' || znak == 'a')
         {
-            drawed = false;
             if(direction == 0 && map[X-1][Y] != '#') X--;
             else if(direction == 1 && map[X][Y-1] != '#') Y--;
-            else if(direction == 2 && map[X-1][Y] != '#') X--;
+            else if(direction == 2 && map[X+1][Y] != '#') X++;
             else if(direction == 3 && map[X][Y+1] != '#') Y++;
             else drawed = true;
         }
         else if(znak == 'D' || znak == 'd')
         {
-            drawed = false;
             if(direction == 0 && map[X+1][Y] != '#') X++;
             else if(direction == 1 && map[X][Y+1] != '#') Y++;
-            else if(direction == 2 && map[X+1][Y] != '#') X++;
+            else if(direction == 2 && map[X-1][Y] != '#') X--;
             else if(direction == 3 && map[X][Y-1] != '#') Y--;
             else drawed = true;
         }
@@ -145,13 +142,18 @@ void walls()
         {
             direction++;
             if(direction == 4) direction = 0;
-            drawed = false;
         }
         else if(znak == 'Q' || znak == 'q')
         {
             direction--;
             if(direction == -1) direction = 3;
-            drawed = false;
+        }
+        else if(znak == 'G' || znak == 'g')
+        {
+            Troda.tp(0, 0);
+            Troda.setColor(0, 15);
+            printf("%i", direction);
+            Sleep(500);
         }
     }
 }
